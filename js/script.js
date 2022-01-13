7f507e0eabad8861a81be7dd984b7f649a3c6deb
David Fanchic Chatelard <davidfchatelard@gmail.com>
2022-01-13 17:58:25 -0300

    Basic JS

---
+++ js/script.js
@@ -0,0 +1,60 @@
+let current_song_image = document.getElementById("current_song_image")
+let current_song_album_photo = document.getElementById("current_song_album_photo")
+let current_song_name = document.getElementById("current_song_name")
+let current_artist_name = document.getElementById("current_artist_name")
+
+
+function somebody_to_love() {
+    current_song_image.src = "../assets/images/queen.jpg"
+    current_song_album_photo.src = "../assets/images/queen.jpg"
+    current_song_name.innerText = "Somebody To Love"
+    current_artist_name.innerText = "Queen"
+}
+
+function bohemian_rhapsody() {
+    current_song_image.src = "../assets/images/queen.jpg"
+    current_song_album_photo.src = "../assets/images/queen.jpg"
+    current_song_name.innerText = "Bohemian Rhapsody"
+    current_artist_name.innerText = "Queen"
+}
+
+function otherside() {
+    current_song_image.src = "../assets/images/red_hot_californiacation.jpg"
+    current_song_album_photo.src = "../assets/images/red_hot_californiacation.jpg"
+    current_song_name.innerText = "Otherside"
+    current_artist_name.innerText = "Red Hot Chili Peppers"
+}
+
+function snow() {
+    current_song_image.src = "../assets/images/red_hot_stadium.jpg"
+    current_song_album_photo.src = "../assets/images/red_hot_stadium.jpg"
+    current_song_name.innerText = "Snow(Hey Oh)"
+    current_artist_name.innerText = "Red Hot Chili Peppers"
+}
+
+function take_what_you_want() {
+    current_song_image.src = "../assets/images/post_malone.jpg"
+    current_song_album_photo.src = "../assets/images/post_malone.jpg"
+    current_song_name.innerText = "Take What You Want"
+    current_artist_name.innerText = "Post Malone"
+}
+
+function random() {
+
+}
+
+function back() {
+
+}
+
+function play_pause() {
+
+}
+
+function next() {
+
+}
+
+function repeat() {
+
+}
